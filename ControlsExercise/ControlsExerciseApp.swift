ed2404ad298b240359e023b714766246a1585a8d
Bruce <bruce@redravencomputing.com>
2023-08-17 23:40:18 -0500

    Initial Commit

---
+++ ControlsExercise/ControlsExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlsExerciseApp.swift
+//  ControlsExercise
+//
+//  Created by Bruce Burgess on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ControlsExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
